38c5d04fe11b1631851ea379b00da4d6b6563dee
AnnGuy1234 <113480126+AnnGuy1234@users.noreply.github.com>
2025-02-10 12:25:37 -0600

    Edited.

--- Board.js
+++ Board.js
@@ -1,1 +1,2 @@
+fetch('http://localhost:8080/get-go-file')
 if (Level +=1) then
--- Cells.go
+++ Cells.go
@@ -1,1 +1,21 @@
+import (
+	"os"
+	"net/http"
+)
 
+os.ReadFile("IMG_0059.png")
+package main
+
+func main() {
+	http.HandleFunc("/get-go-file", func(w http.ResponseWriter, r *http.Request) {
+		content, err := os.ReadFile("Cells.go")
+		if err != nil {
+			http.Error(w, "Error reading file", http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write(content)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
